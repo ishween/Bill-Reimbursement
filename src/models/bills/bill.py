7e698e8a6d0db625a6d7e75595a8e9f985ef1971
Ishween Kaur <ishween1999@gmail.com>
2019-04-07 00:00:32 +0530

    director added

--- src/models/bills/bill.py
+++ src/models/bills/bill.py
@@ -42,7 +42,6 @@ class Bill(object):
             "status": self.status
         }
 
-
     def manager_json(self):
         return {
             "_id": self._id,
@@ -61,6 +60,25 @@ class Bill(object):
     @classmethod
     def get_by_id(cls, _id):
         return cls(**Database.find_one(billConstant.COLLECTION, {'_id': _id}))
+
+    def get_by_id_for_manager(_id):
+        return Database.find_one(billConstant.COLLECTION, {'_id':_id})
+
+    def update(_id, bill_type, employee_id, manager_id, department_id, date_of_submission, bill_image_url, status):
+        response = {}
+        response['bill_type'] = bill_type
+        if employee_id is None:
+            response['manager_id'] = manager_id
+        else:
+            response['employee_id'] = employee_id
+        response['department_id'] = department_id
+        response['date_of_submission'] = date_of_submission
+        response['bill_image_url'] = bill_image_url
+        response['status'] = status
+        # Bill(employee_id, manager_id, bill_type, department_id, date_of_submission,
+        #         bill_image_url, status).manager_update_to_db()
+        Database.update(billConstant.COLLECTION, {'_id':_id}, response)
+
 
     @classmethod
     def all_bills(cls, department_id, status):
@@ -86,6 +104,7 @@ class Bill(object):
         Database.update(billConstant.COLLECTION, {'_id': self._id}, self.employee_json())
 
     def manager_update_to_db(self):
+        print(self.manager_json())
         Database.update(billConstant.COLLECTION, {'_id':self._id}, self.manager_json)
 
     def isReimbursementAdded(reimbursement_amount):
--- src/models/directors/director.py
+++ src/models/directors/director.py
@@ -4,6 +4,7 @@ from src.db.utils import Utils
 import src.models.directors.constants as directorConstants
 import src.models.directors.errors as DirectorErrors
 
+
 class Director(object):
     def __init__(self, company_id, email, password, name, designation, department_id, date_of_joining, _id=None):
         self.company_id = company_id
@@ -13,15 +14,73 @@ class Director(object):
         self.designation = designation
         self.department_id = department_id
         self.date_of_joining = date_of_joining
-        self._id = _id if not None else uuid.uuid4().hex
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     def is_login_valid(email, password):
-        Director_data = Database.find_one(directorConstants.COLLECTION, {'email':email})
+        director_data = Database.find_one(directorConstants.COLLECTION, {'email':email})
 
-        if Director_data is None:
+        if director_data is None:
             raise DirectorErrors.DirectorNOtExistError("You are not registered yet!")
-        if not Utils.check_hashed_password(password, Director_data['password']):
+        if not Utils.check_hashed_password(password, director_data['password']):
             raise DirectorErrors.IncorrectPasswordError("YOu entered wrong password")
         return True
 
-    
+
+    @classmethod
+    def is_reset_password_valid(cls, email, old_password):
+        director_data = Database.find_one(directorConstants.COLLECTION, {'email': email})
+        print(director_data['password'])
+        if not Utils.check_hashed_password(old_password, director_data['password']):
+            raise DirectorErrors.IncorrectPasswordError("Password does not match")
+
+        return cls(**director_data)
+
+    def add_a_director(company_id, email, name, designation, department_id, date_of_joining):
+        password = directorConstants.password_generator()
+        Director(company_id, email, Utils.hash_password(password), name, designation, department_id, date_of_joining).save_to_db()
+        print(password)
+        directorConstants.send_email(email, password)
+
+    def save_to_db(self):
+        Database.insert(directorConstants.COLLECTION, self.json())
+
+    def json(self):
+        return {
+            "_id": self._id,
+            "company_id": self.company_id,
+            "email": self.email,
+            "password": self.password,
+            "name": self.name,
+            "designation": self.designation,
+            "department_id": self.department_id,
+            "date_of_joining": self.date_of_joining
+        }
+
+    def delete(self):
+        print(Database.find_one(directorConstants.COLLECTION, {'_id': self._id}))
+        Database.delete(directorConstants.COLLECTION, {'_id': self._id})
+
+    @classmethod
+    def get_by_id(cls, company_id):
+        return Database.find(directorConstants.COLLECTION, {'company_id': company_id})
+
+    @classmethod
+    def get_by_director_id(cls, _id):
+        return cls(**Database.find_one(directorConstants.COLLECTION, {'_id': _id}))
+
+    @classmethod
+    def all(cls):
+        return [cls(**elem) for elem in Database.find(directorConstants.COLLECTION, {})]
+
+    def update_to_db(self):
+        Database.update(directorConstants.COLLECTION, {'_id':self._id}, self.json())
+
+    @classmethod
+    def get_by_department_id(cls, department_id):
+        directors = Database.find(directorConstants.COLLECTION, {'department_id': department_id})
+        return directors
+
+    @classmethod
+    def get_by_director_email(cls, email):
+        director = Database.find_one(directorConstants.COLLECTION, {'email': email})
+        return director
