fe1e13663d4246622e120fad160a7b17620084a7
Ishween Kaur <ishween1999@gmail.com>
2019-06-02 02:06:38 +0530

    pythonpath setup

--- src/run.py
+++ src/run.py
@@ -1,5 +1,12 @@
 from src.app import app
+import os
+
+print("dekh le andhi ldki: ".upper(),os.environ['PYTHONPATH'])
+
+port = 5000
+if 'PORT' in os.environ:
+    port = os.environ['PORT']
 
 __author__ = 'ishween'
 
-app.run(debug=app.config['DEBUG'], port=5000)
+app.run(debug=app.config['DEBUG'], port=port)
